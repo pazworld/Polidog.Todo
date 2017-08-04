31ed8040e20661183cd5af7505fee9ee4c1e115d
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-08-04 23:27:29 +0900

    add @JsonSchema key prop

--- src/Resource/App/Todo.php
+++ src/Resource/App/Todo.php
@@ -39,7 +39,7 @@ class Todo extends ResourceObject
     }
 
     /**
-     * @JsonSchema(schema="todo.json", params="todo.get.json")
+     * @JsonSchema(key="todo", schema="todo.json", params="todo.get.json")
      */
     public function onGet(string $id) : ResourceObject
     {
