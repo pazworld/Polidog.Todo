e20b813edd07058afd66cd6190b8790045205d3d
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-08-12 18:04:42 +0900

    remove @Curies annotation

--- src/Resource/App/Index.php
+++ src/Resource/App/Index.php
@@ -1,15 +1,16 @@
 <?php
 namespace Polidog\Todo\Resource\App;
 
-use BEAR\Package\Annotation\Curies;
 use BEAR\Resource\ResourceObject;
 
-/**
- * @Curies(name="pt", href="/docs/{?rels}", template=true)
- */
 class Index extends ResourceObject
 {
     public $body = [
+        'curies' => [
+            'href' => '/docs/{?rel}',
+            'name' => 'pt',
+            'templated' => true
+        ],
         'message' => 'Welcome to the Polidog.Todo API ! Our hope is to be as self-documenting and RESTful as possible.',
         '_links' => [
             'pt:todo' => ['href' => '/todo'],
