e2fbff620dfbb7fa4c84e6abc8732225fac3e8ff
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-08-12 11:38:43 +0900

    change message file name for locale

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -29,6 +29,7 @@ class AppModule extends AbstractModule
         $this->install(new NowModule);
         $this->install(new QueryLocatorModule($appDir . '/var/sql'));
         $this->install(new NamedModule(require $appDir . '/var/conf/messages.php'));
+        $this->install(new NamedModule(require $appDir . '/var/locale/en.php'));
         $this->install(new JsonSchemaModule($appDir . '/var/json_schema', $appDir . '/var/json_validate'));
         // Database
         $dbConfig = 'sqlite:' . $appDir . '/var/db/todo.sqlite3';
