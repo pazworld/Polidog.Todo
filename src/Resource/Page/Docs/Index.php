3879b9593fd135dae9b64d56d7684a60f461c217
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-08-12 18:44:36 +0900

    $rel takes without name space

--- src/Resource/Page/Docs/Index.php
+++ src/Resource/Page/Docs/Index.php
@@ -2,6 +2,8 @@
 namespace Polidog\Todo\Resource\Page\Docs;
 
 use BEAR\RepositoryModule\Annotation\Cacheable;
+use BEAR\Resource\Exception\HrefNotFoundException;
+use BEAR\Resource\Exception\ResourceNotFoundException;
 use BEAR\Resource\ResourceObject;
 use BEAR\Sunday\Inject\ResourceInject;
 
@@ -17,7 +19,12 @@ class Index extends ResourceObject
         if ($rel === null) {
             return $this->index();
         }
-        $links = $this->resource->options->uri('app://self/')()->body['_links'];
+        $index = $this->resource->options->uri('app://self/')()->body;
+        $rel = sprintf('%s:%s', $index['curies']['name'], $rel);
+        $links = $index['_links'];
+        if (! isset($links[$rel]['href'])) {
+            throw new ResourceNotFoundException($rel);
+        }
         $href = $links[$rel]['href'];
         $uri = 'app://self' . $href;
         $optionsJson = $this->resource->options->uri($uri)()->view;
@@ -33,9 +40,16 @@ class Index extends ResourceObject
     private function index()
     {
         $index = $this->resource->uri('app://self/index')()->body;
+        $name = $index['curies']['name'];
+        $links = [];
+        foreach ($index['_links'] as $rel => $value) {
+            $newRel = str_replace($name . ':' , '', $rel);
+            $links[$newRel] = $value;
+        }
         $this->body = [
+            'name' => $name,
             'message' => $index['message'],
-            'links' => $index['_links']
+            'links' => $links
         ];
 
         return $this;
