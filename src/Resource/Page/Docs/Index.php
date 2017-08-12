e065720179f8629bb71cdecbd8d414fa027bf461
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-08-12 14:48:29 +0900

    move  /docs to docs/index

--- src/Resource/Page/Docs/Index.php
+++ src/Resource/Page/Docs/Index.php
@@ -1,5 +1,5 @@
 <?php
-namespace Polidog\Todo\Resource\Page;
+namespace Polidog\Todo\Resource\Page\Docs;
 
 use BEAR\RepositoryModule\Annotation\Cacheable;
 use BEAR\Resource\ResourceObject;
@@ -8,7 +8,7 @@ use BEAR\Sunday\Inject\ResourceInject;
 /**
  * @Cacheable(type="view", expiry="never")
  */
-class Docs extends ResourceObject
+class Index extends ResourceObject
 {
     use ResourceInject;
 
