307f1f24582691eddf2aacf69e76d2baa1be5b37
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-08-13 15:02:38 +0900

    content negotiation

--- public/index.php
+++ public/index.php
@@ -1,4 +1,23 @@
 <?php
 
-$context = 'prod-html-app';
+use BEAR\Accept\Accept;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$available = [
+    'Accept' => [
+        'text/html' => 'html-app',
+        'cli' => 'cli-html-app'
+    ],
+    'Accept-Language' => [
+        'ja' => 'ja',
+        'ja-JP' => 'ja',
+        'en' => 'en',
+        'en-US' => 'en',
+        'en-GB' => 'en'
+    ]
+];
+$accept = new Accept($available);
+list($context, $vary) = $accept($_SERVER);
+
 require dirname(__DIR__) . '/bootstrap/bootstrap.php';
