330364741c3dae10cf3890e305ee3e1154612cd5
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-08-13 15:26:02 +0900

    content-negotiation API

--- bootstrap/api.php
+++ bootstrap/api.php
@@ -1,4 +1,24 @@
 <?php
 
-$context = PHP_SAPI === 'cli' ? 'cli-hal-api-app' : 'hal-api-app';
-require __DIR__ . '/bootstrap.php';
+use BEAR\Accept\Accept;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$available = [
+    'Accept' => [
+        'application/hal+json' => 'hal-api-app',
+        'application/json' => 'api-app',
+        'cli' => 'cli-hal-app'
+    ],
+    'Accept-Language' => [
+        'ja' => 'ja',
+        'ja-JP' => 'ja',
+        'en' => 'en',
+        'en-US' => 'en',
+        'en-GB' => 'en'
+    ]
+];
+$accept = new Accept($available);
+list($context, $vary) = $accept($_SERVER);
+
+require dirname(__DIR__) . '/bootstrap/bootstrap.php';
