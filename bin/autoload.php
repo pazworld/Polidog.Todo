0d2009f6b166f7ea655ec0d8108d7a6459b4be33
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-09-23 05:25:13 +0900

    move autoload.php on top

--- bin/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
-$laoder = require dirname(__DIR__) . '/vendor/autoload.php';
-/* @var $loader \Composer\Autoload\ClassLoader */
-AnnotationRegistry::registerLoader([$laoder, 'loadClass']);
